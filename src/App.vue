4c435d4a9c85baba229d66703ed97d4e5e2bb3d2
Senne <>
2022-06-02 17:34:03 +0200

    feat: add font to app

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,9 @@ import Home from "./components/Home.vue";
 
 <style>
 @import url('https://use.typekit.net/kot7xgx.css');
+@import url('https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap');
+
+
 #app {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
