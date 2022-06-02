f6887321189720c76cb7bafdcbb7541331b981c4
Senne <>
2022-06-02 16:42:51 +0200

    feat: upload project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Home from "./components/Home.vue";
+
+
+</script>
+
+<template>
+  <Home/>
+</template>
+
+<style>
+@import url('https://use.typekit.net/kot7xgx.css');
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
