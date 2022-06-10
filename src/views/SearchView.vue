3dcbbec9811da0d41138887afdc2bc102e44c5d8
Senne <>
2022-06-10 15:56:52 +0200

    feat: search

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -8,7 +8,38 @@ let active = "search";
   <input type="text" placeholder="Search" />
 
   <p>Recent searches</p>
-  <Navigation :active = "active" />
+  <div class="flex">
+    <div>
+      <img src="../assets/sandra.svg" alt="sandra" />
+      <div class="name">
+        <h2>Sandra</h2>
+        <h2>More</h2>
+      </div>
+    </div>
+    <div>
+      <img src="../assets/niels.png" alt="Niels" />
+      <div class="name">
+        <h2>Niels</h2>
+        <h2>Verdickt</h2>
+      </div>
+    </div>
+    <div>
+      <img src="../assets/bram.svg" alt="bram" />
+      <div class="name">
+        <h2>Bram</h2>
+        <h2>Colleman</h2>
+      </div>
+    </div>
+    <div>
+      <img src="../assets/johan.svg" alt="johan" />
+      <div class="name">
+        <h2>Johan</h2>
+        <h2>Rossier</h2>
+      </div>
+    </div>
+  </div>
+
+  <Navigation :active="active" />
 </template>
 
 <style scoped>
@@ -32,5 +63,23 @@ p {
   font-weight: 400;
   font-size: 16px;
   color: #ababab;
+}
+
+h2 {
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+}
+
+.name {
+  line-height: 0.3em;
+}
+
+.flex {
+  margin-left: 2em;
+  margin-right: 2em;
+  display: flex;
+  margin-top: 1em;
+  justify-content: space-between;
 }
 </style>
